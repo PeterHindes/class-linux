388b1016ef1dfb0797fcc22a8a0c9a24527b0195
Peter Hindes <gitcommit@s.abad.cc>
2025-02-03 00:19:05 -0700

    Might have fixed a bug in pa1

--- arch/x86/kernel/csci3753_mult.c
+++ arch/x86/kernel/csci3753_mult.c
@@ -5,8 +5,8 @@
 SYSCALL_DEFINE3(csci3753_mult, int, number1, int, number2, long *, result)
 {
   printk(KERN_ALERT "Multiplying '%d' with '%d'!\n", number1, number2);
-  int tmp_result = number1 * number2;
-  if(copy_to_user(result, &tmp_result, sizeof(int)))
+  long tmp_result = number1 * number2;
+  if(copy_to_user(result, &tmp_result, sizeof(long)))
   {
     printk(KERN_ALERT "Failed to copy data to user space\n");
     return -EFAULT;
--- build-install.sh
+++ build-install.sh
@@ -4,7 +4,7 @@
 LOCALVERSION=${1:-}
 
 # Build the kernel with the custom LOCALVERSION
-make CC="ccache gcc" KBUILD_BUILD_TIMESTAMP='' LOCALVERSION="$LOCALVERSION" -j8 && read -p "Press enter to continue"
+time make CC="ccache gcc" KBUILD_BUILD_TIMESTAMP='' LOCALVERSION="$LOCALVERSION" -j8 && read -p "Press enter to continue"
 
 # Install kernel modules and the kernel itself
 sudo make modules_install && sudo make install
